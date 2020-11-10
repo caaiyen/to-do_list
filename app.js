8666e7095fe4b6eb29d135efabf8fb0aeae8a055
caaiyen <ailyn.albores2000@gmail.com>
2020-11-10 10:30:17 +0800

    Added

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser');
+urlEncodedParser = bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+
+let values = [];
+
+app.get("/", (req, res) => {
+    res.render("index", { data: values })
+})
+
+app.get("/delete", (req, res) => {
+    values.pop();
+    res.render("index", { data: values })
+})
+
+app.post("/add", urlEncodedParser, (req, res) => {
+    values.push(req.body)
+    res.redirect("/")
+        // res.status(200).send("connected");
+})
+
+app.listen(PORT, () => {
+    console.log(`Connected to port: ${PORT}`)
+});
